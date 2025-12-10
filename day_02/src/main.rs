d22ed21aadd7a46e38a8eb2d1ed96299cf408487
ferologics <f.hetes@gmail.com>
2025-12-10 17:28:26 +0100

    day 2 pt 2

--- day_02/src/main.rs
+++ day_02/src/main.rs
@@ -24,13 +24,30 @@ fn is_even_digits(x: &u64) -> bool {
 
 fn find_invalid_ids(range: Range) -> Vec<u64> {
     let mut ids = vec![];
-    // find any numbers with some sequence of digits repeated twice (55, 6464, 123123, )
+    // find any numbers with some sequence of digits repeated twice (55, 6464, 123123, ...)
     let even_digit_numbers = (range.start..=range.end).filter(is_even_digits);
     for i in even_digit_numbers {
         let i_string = i.to_string();
         let (start, end) = i_string.split_at(i_string.len() / 2);
         if start == end {
             ids.push(i);
+        }
+    }
+    ids
+}
+
+fn find_invalid_ids_v2(range: Range) -> Vec<u64> {
+    let mut ids: Vec<u64> = vec![];
+    // find any numbers with some sequence of digits repeated AT LEAST twice (55, 6464, 123123, 111, 123123123, ...)
+    for i in range.start..=range.end {
+        let i_string = i.to_string();
+        // find any repeating pattern up to len() / 2
+        for j in 1..=i_string.len() / 2 {
+            let pattern = &i_string[..j];
+            if i_string.split(pattern).all(|p| p.is_empty()) {
+                ids.push(i);
+                break;
+            }
         }
     }
     ids
@@ -43,20 +60,36 @@ fn crack_the_code(input: &str) -> u64 {
     invalid_ids.iter().sum()
 }
 
+fn crack_the_code_v2(input: &str) -> u64 {
+    let id_ranges_split: Vec<&str> = input.split(",").collect();
+    let ranges = id_ranges_split.into_iter().map(Range::new);
+    let invalid_ids: Vec<u64> = ranges.flat_map(find_invalid_ids_v2).collect();
+    invalid_ids.iter().sum()
+}
+
 fn main() {
     let id_ranges_raw = "2157315-2351307,9277418835-9277548385,4316210399-4316270469,5108-10166,872858020-872881548,537939-575851,712-1001,326613-416466,53866-90153,907856-1011878,145-267,806649-874324,6161532344-6161720341,1-19,543444404-543597493,35316486-35418695,20-38,84775309-84908167,197736-309460,112892-187377,336-552,4789179-4964962,726183-793532,595834-656619,1838-3473,3529-5102,48-84,92914229-92940627,65847714-65945664,64090783-64286175,419838-474093,85-113,34939-52753,14849-30381";
-    let answer = crack_the_code(id_ranges_raw);
-    println!("sum of invalid ids: {answer}"); // 29818212493
+    let pt1_answer = crack_the_code(id_ranges_raw);
+    println!("pt 1: {pt1_answer}"); // 29818212493
+    let pt2_answer = crack_the_code_v2(id_ranges_raw);
+    println!("pt 2: {pt2_answer}"); // 29818212493
 }
 
 #[cfg(test)]
 mod tests {
-    use crate::crack_the_code;
+    use crate::{crack_the_code, crack_the_code_v2};
 
     #[test]
-    fn test_example() {
+    fn test_part_1() {
         let id_ranges_raw = "11-22,95-115,998-1012,1188511880-1188511890,222220-222224,1698522-1698528,446443-446449,38593856-38593862,565653-565659,824824821-824824827,2121212118-2121212124";
         let answer = crack_the_code(id_ranges_raw);
         assert_eq!(answer, 1227775554);
+    }
+
+    #[test]
+    fn test_part_2() {
+        let id_ranges_raw = "11-22,95-115,998-1012,1188511880-1188511890,222220-222224,1698522-1698528,446443-446449,38593856-38593862,565653-565659,824824821-824824827,2121212118-2121212124";
+        let answer = crack_the_code_v2(id_ranges_raw);
+        assert_eq!(answer, 4174379265);
     }
 }
